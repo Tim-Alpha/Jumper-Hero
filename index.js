e7282796ecf44c2da750cdd5ad6d76f6ad39cf81
Sachin-Kinha <sachinkinha78@gmail.com>
2023-03-08 10:57:42 +0530

    starting commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+c.fillStyle = 'white'
+c.fillRect(0, 0, 50, 50)
